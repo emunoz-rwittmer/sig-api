0c3a013db33adf5cbf23eb3d4a98c66ade1e6f36
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2024-07-20 20:38:41 -0500

    habilitacion de areas

--- src/controllers/catalogs/departaments.controller.js
+++ src/controllers/catalogs/departaments.controller.js
@@ -15,9 +15,63 @@ const getDepartaments = async (req, res) => {
     }
 }
 
+const getDepartament = async (req, res) => {
+    try {
+        const departamentId = Utils.decode(req.params.departament_id);
+        const result = await DepartamentService.getDepartamentById(departamentId);
+        if (result instanceof Object) {
+            result.id = Utils.encode(result.id);
+        }
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+}
+
+const createDepartament = async (req, res) => {
+    try {
+        const departament = req.body;
+        const result = await DepartamentService.createDepartament(departament);
+        if (result) {
+            res.status(200).json({ data: 'resource created successfully' });
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error.message);
+    }
+}
+
+const updateDepartament = async (req, res) => {
+    try {
+        const departamentId = Utils.decode(req.params.departament_id);
+        const departament = req.body;
+        const result = await DepartamentService.updateDepartament(departament, {
+            where: { id: departamentId },
+        });
+        res.status(200).json({ data: 'resource updated successfully' });
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+}
+
+const deleteDepartament = async (req, res) => {
+    try {
+        const departamentId = Utils.decode(req.params.departament_id);
+        const result = await DepartamentService.delete(departamentId);
+        res.status(200).json({ data: result })
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error.message);
+    }
+}
+
 
 const DepartamentsController = {
-    getDepartaments
+    getDepartaments,
+    getDepartament,
+    createDepartament,
+    updateDepartament,
+    deleteDepartament
 }
 
 module.exports = DepartamentsController 
--- src/services/catalogs/departaments.services.js
+++ src/services/catalogs/departaments.services.js
@@ -11,6 +11,64 @@ class DepartamentService {
             throw error;
         }
     }
+
+    static async getDepartamentById(id) {
+        try {
+            const result = await Departaments.findOne({
+                where: { id },
+                attributes: ['id','name']
+            });
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async getDepartamentsById(arrayIds) {
+        try {
+            const result = await Departaments.findAll({
+                where: { id: {
+                    [Op.in]: arrayIds
+                } },
+                attributes: ['id','name']
+            });
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async createDepartament(departament) {
+        try {
+            const result = await Departaments.create(departament);
+            return result;
+        } catch (error) {
+            throw error;
+
+        }
+    }
+
+    static async updateDepartament(departament, id) {
+        try {
+            const result = await Departaments.update(departament, id);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async delete(departamentId) {
+        try {
+            const result = await Departaments.destroy({
+                where: { id: departamentId }
+            });
+            if(result){
+                return 'resource deleted successfully'
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
 module.exports =  DepartamentService;
