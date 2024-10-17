f131849a3a578ebb78b8b0609242a4ffa79fff85
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2024-10-17 16:24:16 -0500

    end yachtrequest

--- src/controllers/operations/inventory/transactions.controller.js
+++ src/controllers/operations/inventory/transactions.controller.js
@@ -51,64 +51,12 @@ const transactionWarehouse = async (req, res) => {
             userId
         });
         if (transactions) {
-            axios.post('https://5439-190-12-15-164.ngrok-free.app/print/transactions', { products, userName, company })
+            axios.post('http://192.168.1.31:8925/print/transactions', { products, userName, company })
             res.status(200).json({ data: 'transactions register success' });
         }
     } catch (error) {
         console.log(error.message)
         res.status(400).json(error.message);
-    }
-}
-
-const printTransactions = async (req, res) => {
-    try {
-        escpos.USB = require('escpos-usb');
-        const device = new escpos.USB(0x04B8, 0x0202);
-        const printer = new escpos.Printer(device);
-
-        const { products, company, user } = req.body;
-        const formattedDate = new Date();
-        const suma = products.reduce((suma, product) => suma + product.quantity, 0);
-
-        device.open(function () {
-            printer
-                .align('CT')
-                .text(company)
-                .text('')
-                .align('LT')
-                .text(`FECHA: ${Utils.formatDateToLocal(formattedDate)}`)
-                .text(`DESPACHADOR: ${user}`)
-                .text(`TOTAL PRODUCTOS EN GAVETA: ${suma}`)
-                .text('----------------------------------------')
-            printer
-                .align('CT')
-                .tableCustom([
-                    { text: 'CANT', align: "LEFT", width: 0.13 },
-                    { text: 'PRODUCTO', align: "LEFT", width: 0.87 },
-                ])
-
-            products.forEach((product) => {
-                const maxLength = 27; // Define el límite máximo de caracteres
-                const nombreCorto = product.name.length > maxLength
-                    ? product.name.substring(0, maxLength) + '...'
-                    : product.name;
-                printer.tableCustom([
-                    { text: product.quantity.toString(), align: "LEFT", width: 0.13 },
-                    { text: nombreCorto, align: "LEFT", width: 0.87 },
-                ]);
-            });
-
-            printer
-                .text('--------------------------------')
-                .text('EN ESTA GAVETA VAN PRODUCTOS VERIFICADOS Y CONTADOS')
-                .cut()
-                .close();
-
-        });
-        res.status(200).json({ data: 'print success' });
-    } catch (err) {
-        console.error('Error al imprimir:', err);
-        res.status(500).send('Error al imprimir');
     }
 }
 
@@ -141,7 +89,6 @@ const requestWarehouse = async (req, res) => {
 const TransactionController = {
     productEntryInWarehouse,
     transactionWarehouse,
-    printTransactions,
     requestWarehouse
 }
 module.exports = TransactionController
--- src/routes/operations/inventory/transactions.routes.js
+++ src/routes/operations/inventory/transactions.routes.js
@@ -5,7 +5,6 @@ const router = Router();
 
 router.post('/productEntryInWarehouse/:warehouse_id', TransactionController.productEntryInWarehouse);
 router.post('/transactionBetweenWarehouse', TransactionController.transactionWarehouse);
-router.post('/print/transactions', TransactionController.printTransactions);
 // Yacht request 
 router.post('/requestWarehouse', TransactionController.requestWarehouse);
 
