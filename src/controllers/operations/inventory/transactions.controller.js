5b863ea19c787c07184ac37e0b29003a71d5e65e
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2024-11-13 11:02:02 -0500

    upgrades

--- src/controllers/operations/inventory/transactions.controller.js
+++ src/controllers/operations/inventory/transactions.controller.js
@@ -112,7 +112,7 @@ const requestWarehouse = async (req, res) => {
     try {
         const { products, name, status } = req.body;
         products.map(product => {
-            product.productId = Utils.decode(product.productId)
+            product.productId = product.productId
             product.stock = product.stock === '' ? 0 : product.stock
             product.order = product.order === '' ? 0 : product.order
 
