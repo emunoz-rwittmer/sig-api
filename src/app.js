4ae02e09799754bd62d0fa6b8fae5677c0c66a7e
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2024-12-12 11:33:43 -0500

    habilitacion de excel para crews

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const db = require('./utils/database');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(morgan('tiny'));
 
 // linea para servir IMG o PDF
