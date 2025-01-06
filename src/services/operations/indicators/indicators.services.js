1a691abdb2660bd6b5b4b0feaa4585a8ff0d03c1
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2025-01-05 23:01:49 -0500

    seguimiento en roles

--- src/services/operations/indicators/indicators.services.js
+++ src/services/operations/indicators/indicators.services.js
@@ -26,7 +26,7 @@ class IndicatorService {
                     [Sequelize.literal(`(
                         SELECT COUNT(*)
                         FROM indicators AS indicadores
-                        WHERE indicadores.departamentId = Process.id
+                        WHERE indicadores.departament_id = Process.id
                     )`), 'indicatorsCount']
                 ],
                 include: [{
