e2679e3e8ebf198255b0ae35b6cd100d113a1402
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2026-03-16 16:21:39 -0500

    configuracion de formatios

--- src/controllers/rrhh/formats.controller.js
+++ src/controllers/rrhh/formats.controller.js
@@ -226,7 +226,7 @@ const createRequesForStaff = async (req, res) => {
 
         const result = await FormatService.createRequesForStaff(data);
         if (result) {
-            //sendEmailNuevaSolicitud(formatId, dataMail, attachments);
+            sendEmailNuevaSolicitud(formatId, dataMail, attachments);
             res.status(200).json({ data: 'resource created successfully' });
         }
     } catch (error) {
