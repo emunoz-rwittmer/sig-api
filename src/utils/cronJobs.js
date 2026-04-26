8b0a7097b00030203222ca2f714e17ec82c157ac
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2026-04-25 23:15:32 -0500

    activacion de bar

--- src/utils/cronJobs.js
+++ src/utils/cronJobs.js
@@ -8,12 +8,11 @@ cron.schedule('0 0 * * 5', async () => {
     timezone: "America/Guayaquil"
 });
 // Viernes 00:00 crear cruseros e info de pasajeros de la semana venidera de cada yate
-//cron.schedule('0 0 * * 5', async () => {
-// cron.schedule('* * * * *', async () => {
-//     CronJobs.generateWeeklyCruisesAndPassengerInfo();
-// }, {
-//     timezone: "America/Guayaquil"
-// });
+cron.schedule('0 0 * * 5', async () => {
+    CronJobs.generateWeeklyCruisesAndPassengerInfo();
+}, {
+    timezone: "America/Guayaquil"
+});
 //Jueves 10:00 crear evaluaciones para capitanes
 cron.schedule("0 14 * * 4", async () => {
     CronJobs.generateWeeklyEvaluationCrew();
