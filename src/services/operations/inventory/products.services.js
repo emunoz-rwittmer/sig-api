3194470624020abb1b9562197ec39586b9566694
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2026-05-07 23:48:58 -0600

    Hola mundo

--- src/services/operations/inventory/products.services.js
+++ src/services/operations/inventory/products.services.js
@@ -111,7 +111,7 @@ class ProductService {
                     [Sequelize.literal(`
                     (
                         SELECT SUM(CASE 
-                            WHEN transactions.type = 'BAR_CONSUMPTION' AND transactions.warehouse_from_id
+                            WHEN transactions.type = 'BAR_CONSUMPTION' AND transactions.warehouse_from_id = ${warehouseId} 
                             THEN transactions.quantity 
                             ELSE 0 
                         END)
