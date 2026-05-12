ecfe60a12f511b8e3bb15ea8d6e66716ce2c118e
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2026-05-12 16:57:26 -0500

    desactivacion de evalauciones semanales

--- src/utils/cronJobs.js
+++ src/utils/cronJobs.js
@@ -13,9 +13,9 @@ cron.schedule('0 0 * * 5', async () => {
 }, {
     timezone: "America/Guayaquil"
 });
-//Jueves 10:00 crear evaluaciones para capitanes
-cron.schedule("0 14 * * 4", async () => {
-    CronJobs.generateWeeklyEvaluationCrew();
-}, {
-    timezone: "America/Guayaquil"
-});
+// //Jueves 10:00 crear evaluaciones para capitanes
+// cron.schedule("0 14 * * 4", async () => {
+//     CronJobs.generateWeeklyEvaluationCrew();
+// }, {
+//     timezone: "America/Guayaquil"
+// });
