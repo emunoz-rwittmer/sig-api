675f66ab7b3b1dc335602367f4b2fe55d0342ec5
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2026-07-23 21:23:45 -0500

    security: move hashids salt from hardcoded literal to HASHIDS_SALT env var

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,17 +1,16 @@
 const Hashids = require('hashids/cjs')
-const salt = "tiptop-hlfe/r0lf";
 const jwt = require('jsonwebtoken');
 const numberKeys = 10;
 
 class Utils {
   static encode(text) {
-    const hashids = new Hashids(salt, numberKeys);
+    const hashids = new Hashids(process.env.HASHIDS_SALT, numberKeys);
     const id = hashids.encode(text);
     return id;
   }
 
   static decode(text) {
-    const hashids = new Hashids(salt, numberKeys);
+    const hashids = new Hashids(process.env.HASHIDS_SALT, numberKeys);
     const id = hashids.decode(text);
     return id[0];
   }
